c1bd043d1da516faa0c7b694f8b822e69794b2a0
Meral Taşdemir <meraltasdemir77@gmail.com>
2024-11-29 09:24:00 +0300

    Added product entitiy

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Products
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Store { get; set; }
+
+        public Product(Guid id, string name, decimal price, string store)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Store = store;
+        }
+    }
+}

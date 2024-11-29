4370dc8992fb4137654c0cf58edb9304f9c183d6
Meral Taşdemir <meraltasdemir77@gmail.com>
2024-11-29 09:30:21 +0300

    Added Cart and CartItem

---
+++ Domain/Carts/Cart.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Products;
+
+namespace Domain.Carts
+{
+    public class Cart
+    {
+        private readonly List<CartItem> _items = new();
+
+        public IReadOnlyList<CartItem> Items => _items.AsReadOnly();
+
+        public decimal TotalAmount => _items.Sum(item => item.Product.Price * item.Quantity);
+
+        public void AddProduct(Product product, int quantity)
+        {
+            var cartItem = _items.FirstOrDefault(item => item.Product.Id == product.Id);
+            if (cartItem != null)
+            {
+                cartItem.AddQuantity(quantity);
+            }
+            else
+            {
+                _items.Add(new CartItem(product, quantity));
+            }
+        }
+
+        public void RemoveProduct(Guid productId, int quantity)
+        {
+            var cartItem = _items.FirstOrDefault(item => item.Product.Id == productId);
+            if (cartItem == null)
+            {
+                throw new InvalidOperationException("Product not found in the cart.");
+            }
+            cartItem.RemoveQuantity(quantity);
+
+            if (cartItem.Quantity == 0)
+            {
+                _items.Remove(cartItem);
+            }
+        }
+
+    }
+}
---
+++ Domain/Carts/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Products;
+
+namespace Domain.Carts
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public void AddQuantity(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoveQuantity(int quantity)
+        {
+            if (Quantity - quantity < 0)
+                throw new InvalidOperationException("No more items can be removed than are already in the cart.");
+            Quantity -= quantity;
+        }
+    }
+}

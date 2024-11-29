5349f1d0ce01a9130a81329886b7b085a0813715
Meral Taşdemir <meraltasdemir77@gmail.com>
2024-11-29 09:44:31 +0300

    Added CartService

---
+++ Application/CartService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Carts;
+using Domain.Products;
+
+namespace Application
+{
+    public class CartService : ICartService
+    {
+        private readonly Cart _cart;
+
+        public CartService(Cart cart)
+        {
+            _cart = cart;
+        }
+        public void AddProductToCart(Product product, int quantity)
+        {
+            _cart.AddProduct(product, quantity);
+        }
+
+        public void RemoveProductFromCart(Guid productId, int quantity)
+        {
+            _cart.RemoveProduct(productId, quantity);
+        }
+
+        public IReadOnlyList<CartItem> GetCart()
+        {
+            return _cart.Items;
+        }
+
+        public decimal GetTotalAmount()
+        {
+            return _cart.TotalAmount;
+        }
+    }
+}
---
+++ Application/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Carts;
+using Domain.Products;
+
+namespace Application
+{
+    public interface ICartService
+    {
+        void AddProductToCart(Product product, int quantity);
+        void RemoveProductFromCart(Guid productId, int quantity);
+        IReadOnlyList<CartItem> GetCart();
+        decimal GetTotalAmount();
+    }
+}
